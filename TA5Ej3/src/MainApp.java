40cf46aeb8197734246a969b81b0b85aabf400e4
Marccudi <marccudi@hotmail.es>
2022-07-07 09:40:21 +0200

    Ejercicio 3 acabado

---
+++ TA5Ej3/src/MainApp.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String nombre = JOptionPane.showInputDialog("Introduce tu nombre:");
+		System.out.println("Bienvenido " + nombre);
+	}
+
+}
