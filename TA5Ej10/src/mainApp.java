1d951043e295aba3bb00f71468a543b6eb732335
Marccudi <marccudi@hotmail.es>
2022-07-07 10:16:44 +0200

    Ejercicio 5 acabado

---
+++ TA5Ej10/src/mainApp.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+
+public class mainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String numero=JOptionPane.showInputDialog("Numero de ventas a introducir:");
+		int numeroVentas=Integer.parseInt(numero);
+		double result = 0;
+		double venta = 0;
+		for (int i = 1; i <= numeroVentas; i++) {
+			String a = JOptionPane.showInputDialog("Precio de la venta numero " + i);
+			venta += Double.parseDouble(a);
+		}
+		System.out.println("El total de las ventas es de: " + venta);
+	}
+
+}
