eab1de0636581086e9d3045cf972966cab4124bf
Marccudi <marccudi@hotmail.es>
2022-07-07 10:03:00 +0200

    Ejercicio 7 acabado

---
+++ TA5Ej7/src/mainApp.java
@@ -0,0 +1,13 @@
+
+public class mainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=1;
+		while (num<=100) {
+			System.out.println(num);
+			num++;
+		}
+	}
+
+}
