76b933200602a31a284f7f61cd8319fc2209f7c2
Marccudi <marccudi@hotmail.es>
2022-07-07 10:17:18 +0200

    Ejercicio 10 acabado

--- TA5Ej4/src/MainApp.java
+++ TA5Ej4/src/MainApp.java
@@ -6,7 +6,7 @@ public class MainApp {
 		// TODO Auto-generated method stub
 		String numero = JOptionPane.showInputDialog("Introduce el radio del circulo:");
 		Double radio = Double.parseDouble(numero);
-		Double result = Math.PI * Math.pow(radio, 2);
+		Double result = Math.PI * Math.pow(radio, 2); 
 		System.out.println("El area del circulo con un radio de " + radio + " es de: " + result);
 
 	}
