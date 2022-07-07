abb1e47650fb8106a7e5863cfba0574281cf3f95
Marccudi <marccudi@hotmail.es>
2022-07-07 09:19:58 +0200

    Ejercicio 2 acabado

---
+++ TA5Ej2/src/MainApp.java
@@ -0,0 +1,10 @@
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nombre = "Marc";
+		System.out.println("Bienvenido " + nombre);
+	}
+
+}
