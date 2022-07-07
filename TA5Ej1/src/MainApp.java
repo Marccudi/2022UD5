ea3ea467fd9b80d838e6950ec08bb2fa076aaaff
Marccudi <marccudi@hotmail.es>
2022-07-07 09:18:00 +0200

    Ejercicio 1 acabado

---
+++ TA5Ej1/src/MainApp.java
@@ -0,0 +1,18 @@
+                  
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 14;
+		int b = 35;
+		
+		if (a<b) {
+			System.out.println("el numero " + a + " es menor que el numero " + b);
+		} else if(a>b){
+			System.out.println("el numero " + a + " es mayor que el numero " + b);
+		}else {
+			System.out.println("Los numeros " + a + " y " + b + " son iguales");
+		}
+	}
+
+}
